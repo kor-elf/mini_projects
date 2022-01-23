ad5c6301abdec767c5cb0728e5510c8cb9689c7f
Leonid Nikitin <i@kor-elf.net>
2022-01-23 13:01:32 +0600

    Added migration add table products.

---
+++ database/migrations/2022_01_23_065113_projects.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Projects extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('art')->unique();
+            $table->string('name');
+            $table->string('status')->index();
+            $table->jsonb('data');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
