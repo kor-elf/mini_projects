a271d9cfe6136f03ebc47da13dbbc008d7335050
Leonid Nikitin <i@kor-elf.net>
2022-01-23 22:54:37 +0600

    Added notification CreatedProduct.

---
+++ app/Notifications/CreatedProduct.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\Models\Product;
+
+class CreatedProduct extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    private Product $product;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $url = '/admin/products/' . $this->product->id . '/edit/';
+
+        return (new MailMessage)
+                    ->line('Created product.')
+                    ->line('Art: ' . $this->product->art . '.')
+                    ->line('Name: ' . $this->product->name . '.')
+                    ->action('Link', url($url));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
